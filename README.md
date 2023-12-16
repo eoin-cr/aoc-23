6f48f2b1241dbee7be39b83e278a1e0740307ccd
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-12-16 12:17:13 +0000

    Label map is now being generated correctly

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AOC 2023
-I will be trying to code my solutions entirely through functional programming in Scala this year. Writing in a purely functional style will take preference over performance, hence why some of the programs may not be as efficient as possible
+I will be trying to code my solutions entirely through functional programming in Scala this year. Writing in a purely functional style will take preference over performance, hence why some of the programs may not be as efficient (or readable) as possible
--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -1,21 +1,48 @@
+import scala.annotation.tailrec
+
 object Day15:
   def soln1(lst: String): Int =
     lst.split(',').foldLeft(0)(_ + _.foldLeft(0)((l,r) => (l + r.toInt)*17 %256))
 
   def soln2(str: String): Int =
-//    def calculateHash(s: String): Int = s.split("=")(0).foldLeft(0)((l, r) => (l + r.toInt)*17%265)
-
-//    def hash(str: String): Int = str.foldLeft(0)((l, r) => (l + r.toInt) * 17 % 265)
-//    def calculateHash(s: String): Int = hash(s.split("=")(0))
     def calculateHash(s: String): Int = soln1(s.split("=")(0))
+//    @tailrec
+    def replaceInList(s: String, newVal: Int, lst: List[(String, Int)], acc: List[(String, Int)]=List()): List[(String, Int)] =
+      lst match
+        case x :: xs if x(0) == s => acc.reverse ::: ((s, newVal) :: xs)
+        case x :: xs => replaceInList(s, newVal, xs, x :: acc)
+        case Nil if acc == Nil => List((s, newVal))
+        case Nil => ((s, newVal) :: acc).reverse
 
-    val map: Map[Int, List[Int]] = Map().withDefaultValue(List())
+    def removeFromList(s: String, lst: List[(String, Int)], acc: List[(String, Int)]=List()): List[(String, Int)] =
+      lst match
+        case x :: xs if x(0) == s => acc.reverse ::: xs
+        case x :: xs => removeFromList(s, xs, x :: acc)
+        case Nil => acc.reverse
+
+
+    val map: Map[Int, List[(String, Int)]] = Map().withDefaultValue(List())
 
     str.split(',')
-      .foldLeft(map){(m, s) =>
+      .foldLeft(map) { (m, s) =>
         if s.contains("=") then
-          m + (calculateHash(s) -> (m(calculateHash(s)) :+ s.split("=")(1).toInt))
-        else m(calculateHash(s.split("-")(0))) match
-          case x :: xs => m + (calculateHash(s.split("-")(0)) -> xs)
-          case Nil => m
-      }.map((k, v) => v.reverse.foldLeft((1, 0))((l, r) => (l(0)+1, l(0)+r*l(0)))(1)).sum
+          val label = s.split("=")(0)
+          val lens = s.split("=")(1).toInt
+
+          m + (calculateHash(label) -> replaceInList(label, lens, m(calculateHash(label))))
+        else
+          val label = s.split("-")(0)
+          val labelHash = calculateHash(label)
+          m + (labelHash -> removeFromList(label, m(labelHash)))
+      }
+//      .map((k, v) => v.reverse.foldLeft((1, 0))((l, r) => (l(0) + 1, l(0) + r * l(0)))(1)).sum
+    10
+
+//    str.split(',')
+//      .foldLeft(map){(m, s) =>
+//        if s.contains("=") then
+//          m + (calculateHash(s) -> (m(calculateHash(s)) :+ s.split("=")(1).toInt))
+//        else m(calculateHash(s.split("-")(0))) match
+//          case x :: xs => m + (calculateHash(s.split("-")(0)) -> xs)
+//          case Nil => m
+//      }.map((k, v) => v.reverse.foldLeft((1, 0))((l, r) => (l(0)+1, l(0)+r*l(0)))(1)).sum
